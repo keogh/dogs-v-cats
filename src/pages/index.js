e566e3c6dfdbeab893adc3003dd25636dfe9d5bb
Isaac Zepeda <keogh24@gmail.com>
2020-02-26 18:41:44 -0700

    Added footer notes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import React, { useState, useRef, useEffect } from "react"
-// import { Link } from "gatsby"
-import * as tf from '@tensorflow/tfjs';
-
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Link from '@material-ui/core/Link';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import PetsIcon from '@material-ui/icons/Pets';
-
-import Layout from "../components/layout"
+// import { Link } from "gatsby"
+import * as tf from '@tensorflow/tfjs';
+import React, { useEffect, useRef, useState } from "react";
+import Layout from "../components/layout";
 // import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/seo";
 
 
 const useModel = url => {
@@ -30,8 +30,8 @@ const useModel = url => {
 
 // TODO: Migrate to TailwingCSS
 const useStyles = makeStyles(() => ({
-  predictedPet: {
-    marginBottom: 12,
+  someMarginBottom: {
+    marginBottom: 16,
   }
 }));
 
@@ -88,6 +88,12 @@ const IndexPage = ({ location }) => {
       <SEO title="Home" />
       <CssBaseline />
       <div>
+        <div className={classes.someMarginBottom}>
+          <Typography gutterBottom variant="h6">
+            This a image classifier that tries to predict whether the selected image is a Dog or Cat.
+          </Typography>
+        </div>
+
         <Typography paragraph>
           Select a image:&nbsp;
           <input
@@ -110,7 +116,7 @@ const IndexPage = ({ location }) => {
         </p>
         {predictedData && (
           <>
-            <div className={classes.predictedPet}>
+            <div className={classes.someMarginBottom}>
               <Typography display="inline" variant="h5">
                 This is a&nbsp;
               </Typography>
@@ -132,6 +138,26 @@ const IndexPage = ({ location }) => {
             <img id="img-sample" src={imageSrc} ref={imageEl} alt="preview" style={{ maxHeight: 400 }} />
           }
         </p>
+        <Divider className={classes.someMarginBottom} />
+        <div>
+          <Typography gutterBottom variant="body2">
+            This model was trained with <Link href="https://keras.io/" target="_blank" rel="noopener">Keras</Link> library in python with <Link href="https://tensorflow.org/" target="_blank" rel="noopener">Tensorflow</Link> as backend.
+            <br/><br/>
+            I used the <Link href="https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition" target="_blank" rel="noopener">"Dogs vs Cats Redux: Kernels Edition"</Link> dataset un <Link href="https://www.kaggle.com/" target="_blank" rel="noopener">Kaggle</Link>.
+            <br/><br/>
+            I trained it using <Link href="http://cs231n.github.io/convolutional-networks/" target="_blank" rel="noopener">convolutional neural network</Link> with a <Link href="https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html" target="_blank" rel="noopener">data augmentation</Link> technique based on my readings in <Link href="https://twitter.com/fchollet" target="_blank" rel="noopener">François Chollet's</Link> book <Link href="https://www.goodreads.com/book/show/33986067-deep-learning-with-python" target="_blank" rel="noopener">Deep Learning with Python</Link>.
+            <br/><br/>
+            You can see all the data preparation, training, evaluation, test, accuracy in the Kaggle notebook: <Link href="https://www.kaggle.com/keogh24/dogs-vs-cats-keras-data-augmentation/" target="_blank" rel="noopener">https://www.kaggle.com/keogh24/dogs-vs-cats-keras-data-augmentation/</Link>
+            <br/><br/>
+            I saved the model in <Link href="https://www.h5py.org/" target="_blank" rel="noopener">HDF5</Link> format then <Link href="https://www.tensorflow.org/js/guide/conversion" target="_blank" rel="noopener">I exported to tensorflow.js</Link>, loaded it in a <Link href="https://www.gatsbyjs.org/" target="_blank" rel="noopener">gatsby.js</Link> static website.
+            <br/><br/>
+            This will only try to predict whether the image is a dog or cat no matter what image you upload.
+            <br/><br/>
+            This is all done in the web browser using <Link href="https://www.tensorflow.org/js" target="_blank" rel="noopener">tensorflow.js</Link>
+            <br/><br/>
+            This was a site I created for a tech talk I gave in Feb 27 2020.
+          </Typography>
+        </div>
       </div>
     </Layout>
   );
